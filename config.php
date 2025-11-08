0a57cc984f029a5e4857794fe291b57bd06112b0
sdssatos25-max <sdssatos25@gmail.com>
2025-11-08 18:09:06 -0300

    s

--- config.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('ADMIN_USER', 'sucesso777lojas???');
-define('ADMIN_PASS_HASH', 'e560127d4aaf0dd1e8025e59802a727994060d44051addf0e43daba911216e95');
-?>
--- get_metrics.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = new PDO('sqlite:' . __DIR__ . '/cloaker.db');
-$metricsStmt = $db->query("SELECT * FROM metrics");
-$metrics = $metricsStmt->fetch(PDO::FETCH_ASSOC);
-$metrics['sources'] = json_decode($metrics['sources'], true) ?? [];
-header('Content-Type: application/json');
-echo json_encode($metrics);
-?>
